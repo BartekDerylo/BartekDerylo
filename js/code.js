be63f145e341dea7dc3e86b69a3446723e30b5c1
Bartosz Derylo <bartekderylo@gmail.com>
2020-09-06 12:14:42 +0200

    CSS main style WIP, JS add main__section active

--- js/code.js
+++ js/code.js
@@ -6,3 +6,17 @@ const navList = document.querySelector("#nav__list");
 navButton.addEventListener("click", () => {
   navList.classList.toggle("ride");
 });
+
+// Main's section show
+
+const navItems = document.querySelectorAll(".nav__item");
+const mainSections = document.querySelectorAll(".main__section");
+
+for (let i = 0; i < navItems.length; i++) {
+  navItems[i].addEventListener("click", function () {
+    mainSections.forEach((mainSection) => {
+      mainSection.classList.remove("active");
+    });
+    mainSections[i].classList.add("active");
+  });
+}
